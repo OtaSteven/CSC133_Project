3c0fb6e6df6fec8c47f869b3f795356f69ac9845
OtaSteven <sho8517@gmail.com>
2022-10-27 17:14:50 -0700

    Manage to flip the screen to where 0,0 is at the bottom left. Started working on animating my pond and added my update() method to every class that implements Updatable.

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,5 +1,9 @@
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -8,19 +12,36 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 import java.util.Random;
 
+interface Updatable
+{
+  void update();
+}
 class Game extends Pane
 {
   public Game()
   {
+    setScaleY(-1);
+    getChildren().add(new Pond());
     getChildren().add(new HeliPad());
     getChildren().add(new Helicopter());
   }
 }
-abstract class GameObject extends Group
+abstract class GameObject extends Group implements Updatable
 {
+  public void update()
+  {
+    for (Node n : getChildren())
+    {
+      if (n instanceof Updatable)
+        ((Updatable)n).update();
+    }
+  }
+  public void add(Node node)
+  {
+    getChildren().add(node);
+  }
 }
 class Pond extends GameObject
 {
@@ -30,8 +51,24 @@ class Pond extends GameObject
   public Pond()
   {
     rand = new Random();
-    pond = new Circle(25, Color.BLUE);
-    pond.setCenterX(rand.nextInt((int)(GameApp.WIDTH)));
+    pond = new Circle();
+    pond.setFill(Color.BLUE);
+    pond.setRadius(25);
+    pond.setCenterY(150);
+    pond.setCenterX(150);
+    update();
+    add(pond);
+  }
+
+  @Override
+  public void update() {
+    AnimationTimer loop = new AnimationTimer() {
+      @Override
+      public void handle(long now) {
+        pond.setTranslateX(pond.getTranslateX()+1);
+      }
+    };
+    loop.start();
   }
 }
 class Cloud extends GameObject
@@ -44,6 +81,12 @@ class Cloud extends GameObject
     rand = new Random();
     cloud = new Circle(25, Color.BLUE);
     cloud.setCenterX(rand.nextInt((int)(GameApp.WIDTH)));
+    add(cloud);
+  }
+
+  @Override
+  public void update() {
+
   }
 }
 class HeliPad extends GameObject
@@ -56,13 +99,20 @@ class HeliPad extends GameObject
     heliPad = new Rectangle(75,75);
     heliPad.setStroke(Color.GRAY);
     heliPad.setX((GameApp.WIDTH/2)-(heliPad.getWidth()/2));
-    heliPad.setY(GameApp.HEIGHT-100);
+    heliPad.setY(100);
+
     padCircle = new Circle();
     padCircle.setRadius(30);
     padCircle.setStroke(Color.YELLOW);
     padCircle.setCenterX(heliPad.getX()+heliPad.getWidth()/2);
     padCircle.setCenterY(heliPad.getY()+heliPad.getHeight()/2);
-    getChildren().addAll(heliPad, padCircle);
+    add(heliPad);
+    add(padCircle);
+  }
+
+  @Override
+  public void update() {
+
   }
 }
 class Helicopter extends GameObject
@@ -74,23 +124,26 @@ class Helicopter extends GameObject
     heli = new Circle(10);
     heli.setFill(Color.YELLOW);
     heli.setCenterX((GameApp.WIDTH/2));
-    heli.setCenterY((GameApp.HEIGHT)-60);
+    heli.setCenterY(130);
     heliHead = new Line(heli.getCenterX(), heli.getCenterY(), heli.getCenterX(),
-        heli.getCenterY()-25);
+        heli.getCenterY()+25);
     heliHead.setStroke(Color.YELLOW);
-    getChildren().addAll(heli, heliHead);
+    add(heli);
+    add(heliHead);
+  }
+
+  @Override
+  public void update() {
+
   }
 }
-class PondAndCloud extends GameObject
-{
+class PondAndCloud extends GameObject {
 
+  @Override
+  public void update() {
+
+  }
 }
-interface Updatable
-{
-
-}
-
-
 public class GameApp extends Application {
 
   final static double WIDTH = 600;
@@ -103,7 +156,6 @@ public class GameApp extends Application {
     Game game = new Game();
     Scene scene = new Scene(game, WIDTH, HEIGHT);
     scene.setFill(Color.BLACK);
-
     primaryStage.setScene(scene);
     primaryStage.setTitle("RainMaker");
     primaryStage.setResizable(false);
